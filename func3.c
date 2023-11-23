916870465c3c3ebf68a7f6daf20d5ab6d5fd002c
chayanne <chayanne.morango02@gmail.com>
2023-11-23 12:06:22 -0300

    Do-while, Array, Funções

---
+++ func3.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include "mat.h"
+
+int main(){
+system("clear");
+int num[10]={12,8,17,90,3,7,5,18,45,6};
+printf("A soma dos valores é %d\n",soma(num,10));
+printf("O maior valor é %d\n",max(num,10));
+return 0;
+};
